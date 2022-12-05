5f05971eb54a48360f6ba9352ca3892e0ea0272c
Indra Faozi <indrafaozi00@gmail.com>
2022-12-05 19:36:19 +0700

    Add some validation

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -6,6 +6,7 @@ use App\Models\Mahasiswa;
 use App\Models\SumberInformasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Carbon;
 
 class MahasiswaController extends Controller
 {
@@ -42,34 +43,52 @@ class MahasiswaController extends Controller
      */
     public function store(Request $request)
     {
+        $no_telpon = [
+            '0852',
+            '0853',
+            '0811',
+            '0812',
+            '0813',
+            '0821',
+            '0822',
+            '0851',
+        ];
+        
+        $str_no_telepon = implode(',', $no_telpon);
+        
         $rules = [
-            'nama_calon_mahasiswa' => 'required|regex:/^[\pL\s\-]+$/u',
-            'nisn' => 'required|numeric',
+            'nama_calon_mahasiswa' => 'required|regex:/^[\pL\s\-]+$/u|digits_between:2,20',
+            'nisn' => 'required|numeric|digits:10',
             'jenis_kartu_identitas' => 'required',
-            'no_kartu_identitas' => 'required|numeric',
+            'no_kartu_identitas' => 'required|numeric|digits_between:12,16',
             'jenis_kelamin' => 'required',
             'agama' => 'required',
-            'tempat_lahir' => 'required|regex:/^[\pL\s\-]+$/u',
-            'tanggal_lahir' => 'required|date',
+            'tempat_lahir' => 'required|regex:/^[\pL\s\-]+$/u|digits_between:2,20',
+            'tanggal_lahir' => 'required|date|before_or_equal:' . Carbon::now()->subYears(18)->format('Y-m-d'),
             'alamat_lengkap' => 'required',
-            'no_wa' => 'required|numeric',
+            'no_wa' => 'required|numeric|starts_with:'.$str_no_telepon,
             'email' => 'required|email',
-            'no_telepon_wali' => 'required|numeric',
+            'no_telepon_wali' => 'required|numeric|starts_with:'.$str_no_telepon,
             'nama_pemberi_referensi' => 'required|regex:/^[\pL\s\-]+$/u',
             'sumber_informasi' => 'required',
         ];
 
         $messages = [
+            "nama_calon_mahasiswa.digits_between" => "Minimal huruf :min dan maksimal huruf :max",
             "nama_calon_mahasiswa.required" => "Nama Calon Mahasiswa harus diisi",
             "nisn.required" => "NISN harus diisi",
+            "nisn.digits" => "NISN harus berisi :digits digit",
             "nisn.numeric" => "NISN harus berupa angka",
             "no_kartu_identitas.required" => "No Kartu Identitas harus diisi",
+            "no_kartu_identitas.digits_between" => "Minimal angka :min dan maksimal angka :max",
             "no_kartu_identitas.numeric" => "No Kartu Identitas harus berupa angka",
             "jenis_kartu_identitas.required" => "Jenis Kartu Identitas harus dipilih",
             "jenis_kelamin.required" => "Jenis Kelamin harus dipilih",
             "agama.required" => "Agama harus dipilih",
             "tempat_lahir.required" => "Tempat Lahir harus diisi",
+            "tempat_lahir.digits_between" => "Minimal huruf :min dan maksimal huruf :max",
             "tanggal_lahir.required" => "Tanggal Lahir harus diisi",
+            "tanggal_lahir.before_or_equal" => "Usia harus 18 tahun",
             "alamat_lengkap.required" => "Alamat Lengkap harus diisi",
             "email.required" => "Alamat Email harus diisi",
             "email.email" => "Masukkan format email yang benar",
@@ -92,13 +111,13 @@ class MahasiswaController extends Controller
         $str_sumber_informasi = implode(',', $request->sumber_informasi);
         $request->merge(['sumber_informasi' => $str_sumber_informasi]);
 
-        Mahasiswa::create($request->all());
+        $mahasiswa = Mahasiswa::create($request->all());
 
         $data = [
-            'data' => $request->all()
+            'email' => $mahasiswa->email
         ];
 
-        // return view('mahasiswa.result', $data);
+        return redirect()->route('mahasiswas.show', $data['email']);
     }
 
     /**
